29a49047f400024a89cc46054b2d3b4e67af6487
toxicial <73919935+toxicial@users.noreply.github.com>
2021-11-15 22:02:47 -0800

    api sucks

--- src/js/settings/playerHighlight.js
+++ src/js/settings/playerHighlight.js
@@ -1,3 +1,8 @@
+const toastify = require('toastify-js')
+const api = require('../js/utils/api')
+
+const Mojang = new api.Mojang()
+
 window.addEventListener("load", () => {
     let button = document.querySelector(".playerHighlightButton")
 
@@ -33,25 +38,45 @@ function highlightPromt () {
 
     let cancelButton = document.querySelector(".highlightPromtCancel")
     let submitButton = document.querySelector(".highlightPromtSubmit")
+    let inputBar = document.getElementById("highlightPromtInput")
 
     cancelButton.addEventListener("click", () => {
         body.innerHTML = ""
     })
 
-    submitButton.addEventListener("click", () => {
+    inputBar.addEventListener("keypress", e => {
+        if (e.key == "Enter") return submitButton.click()
+    })
+    
+    submitButton.addEventListener("click", async () => {
         let input = document.getElementById("highlightPromtInput").value
 
         body.innerHTML = ""
+
+        let response = await Mojang.getMojang(input)
+
+        if (!response) console.log("no response")
 
         let players = localStorage.read("highlightedPlayers") ?? []
 
         let stat = false
 
         players.forEach(player => {
-            if (player.uuid == input) return stat = true
+            if (player.uuid == response.id) return stat = true
         })
 
-        if (stat == false) players.push({"uuid": input, "ign": input, "head": input})
+        if (stat == false) players.push({"uuid": response.id, "ign": response.name, "head": `https://crafatar.com/avatars/${response.id}?size=64.png`})
+
+        if (stat == true) {
+            toastify({
+                text: "This player is already highlighted",
+                duration: 3000,
+                className: "toastWarning",
+                position: "left",
+                gravity: "bottom",
+                stopOnFocus: true,
+              }).showToast();
+        }
 
         localStorage.write("highlightedPlayers", players)
     })
---
+++ src/js/utils/api.js
@@ -0,0 +1,39 @@
+const fetch = require('node-fetch')
+
+class Mojang {
+    constructor () {
+
+    }
+
+        /**
+     * returns mojang api
+     * @param {string} username - players in game name
+     * @returns the given players mojang stats
+     */
+         async getMojang (username) {
+            const response = await fetch(`https://api.mojang.com/users/profiles/minecraft/${username}`)
+    
+            if (response.statusCode != 200) return Promise.reject(`${username} does not exsist`)
+
+            const json = await response.json()
+
+            return json
+        }
+
+    /**
+     * converts players name to uuid
+     * @param {string} username - players in game name
+     * @returns the given players uuid 
+     */
+    async getUUID (username) {
+        const response = await fetch(`https://api.mojang.com/users/profiles/minecraft/${username}`)
+
+        const json = await response.json()
+
+        return {"response": json.id, "statusCode": response.status}
+    }
+}
+
+module.exports = {
+    Mojang: Mojang
+}
