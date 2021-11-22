11a5c2c50aac77969bf7d99d3f47b8c8d4e29006
toxicial <73919935+toxicial@users.noreply.github.com>
2021-11-21 20:27:32 -0800

    comment

--- src/js/settings/selectors.js
+++ src/js/settings/selectors.js
@@ -1,9 +1,54 @@
+const { dialog } = require('electron').remote
+const dialogOptions = require('../js/utils/json/dialogOptions.json')
+
 window.addEventListener("load", () => {
     let input = document.getElementById("logInput")
     let options = document.querySelectorAll(".logSelector-content a")
     let content = document.querySelector(".logSelector-content")
 
     let data = localStorage.read("client") ?? ""
+
+    //     let select = document.getElementById("clientSettings")
+
+    // let blc = path.join(mcPath, "blclient/minecraft").replaceAll("\\", "/")
+    // let lc = path.join(os.homedir(), "/.lunarclient/offline/1.8/logs").replaceAll("\\", "/")
+    // let plc = path.join(mcPath, "../../.pvplounge/logs").replaceAll("\\", "/")
+    // let vf = mcPath.replaceAll("\\", "/")
+
+
+    // let logPath = ""
+    // let client = ""
+
+
+    // select.value = localStorage.read("client") ?? "vf"
+
+
+    // select.addEventListener("change", event => {
+    //     let target = event.target.value
+        
+    //     if (target == "blc") logPath = blc, client = "blc"
+    //     if (target == "lc") logPath = lc, client = "lc"
+    //     if (target == "plc") logPath = plc, client = "plc"
+    //     if (target == "vf") logPath = vf , client = "vf"
+
+    //     localStorage.write("logPath", logPath)
+    //     localStorage.write("client", client)
+    // })
+
+    // manual client picker
+// window.addEventListener("load", () => {
+//     let button = document.getElementById("manualSelectLog")
+
+//     button.addEventListener("click", async () => {
+//         if (process.platform !== 'darwin') dialogOptions.properties.push('OpenDirectory')
+
+//         let file = await dialog.showOpenDialog(dialogOptions)
+
+//         let path = file.filePaths[0].replace(/\\/g, "\/")
+
+//         localStorage.write("logPath", path)
+//     })
+// })
 
     input.value = data
 
